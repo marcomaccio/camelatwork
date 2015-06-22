472b75558c12fc02a72c2537df4a3db04823f682
Marco Maccio <marco.maccio@marmac.name>
2015-06-22 14:49:55 +0200

    Changed the Repository ID
    Changed the Repository id in order to publish the build by Jenkins

---
+++ components/bindy/src/main/java/com/camelatwork/components/bindy/routes/BindyRoute.java
@@ -0,0 +1,33 @@
+package com.camelatwork.components.bindy.routes;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by marcomaccio on 11/06/2015.
+ */
+public class BindyRoute extends RouteBuilder
+{
+    private static final transient Logger LOGGER = LoggerFactory.getLogger(BindyRoute.class);
+
+    BindyCsvDataFormat formatBCVExport = new BindyCsvDataFormat(com.camelatwork.components.bindy.model.BCVBankExport.class);
+
+    @Override
+    public void configure() throws Exception {
+
+
+        from("file:src/main/data/csv?noop=true&delay=10")
+            .routeId("Bindy :: BCV Import Service ")
+                .log(LoggingLevel.DEBUG, "${body}  ")
+                .split().tokenize("\n", 5).streaming()
+                    .log(LoggingLevel.DEBUG, "${body}  ")
+                    .unmarshal(formatBCVExport)
+
+                .end()    ;
+
+    }
+
+}
